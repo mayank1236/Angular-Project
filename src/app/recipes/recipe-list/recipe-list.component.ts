801d3749d327754d64ec94bd372bfe9132b636b0
Mayank <mayankgupta14042000@gmail.com>
2022-03-26 14:22:34 +0530

    Features related to Ingredients

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,20 +8,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe('A Sandwich', 'A Vegetarian vegatabe and paneer sandwich', 'https://www.mashed.com/img/gallery/chain-sandwich-shops-ranked-from-worst-to-best/intro-1625663662.webp'),
-    new Recipe('Another Sandwich', 'A Vegetarian vegatabe and paneer sandwich', 'https://www.mashed.com/img/gallery/chain-sandwich-shops-ranked-from-worst-to-best/intro-1625663662.webp')
-  ];
+  constructor(private recipeService: RecipeService) { 
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,36 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
     
+    private recipes: Recipe[] = [
+        new Recipe('A Sandwich', 
+        'A Vegetarian vegatabe and paneer sandwich', 
+        'https://www.mashed.com/img/gallery/chain-sandwich-shops-ranked-from-worst-to-best/intro-1625663662.webp',
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20),
+        ]),
+        new Recipe('Another Sandwich', 
+        'A Vegetarian vegatabe and paneer sandwich', 
+        'https://www.mashed.com/img/gallery/chain-sandwich-shops-ranked-from-worst-to-best/intro-1625663662.webp',
+        [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 1),
+        ])
+    ];
+
+    constructor(private slService: ShoppingListService) {}
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+    }
 }
