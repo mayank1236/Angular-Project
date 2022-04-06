1a7c138848100ad62dea355ed0486574c4725ba3
Mayank <mayankgupta14042000@gmail.com>
2022-04-06 22:04:39 +0530

    added the login auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { AuthService } from "./auth.service";
+import { Observable } from "rxjs";
+import { AuthResponseData, AuthService } from "./auth.service";
 
 @Component({
     selector: 'app-auth',
@@ -25,22 +26,26 @@ export class AuthComponent {
         const email = form.value.email;
         const password = form.value.password;
 
+        let authObs: Observable<AuthResponseData>;
+
         this.isLoading = true;
         if (this.isLoginMode) {
-
+            authObs = this.authService.login(email, password);
         } else {
-            this.authService.signup(email, password).subscribe(
-                restData => {
-                    console.log(restData);
-                    this.isLoading = false;
-                },
-                error => {
-                    this.error = 'An error occured!';
-                    this.isLoading = false;
-                }
-            );
+            authObs = this.authService.signup(email, password);
         }
-        
+
+        authObs.subscribe(
+            restData => {
+                console.log(restData);
+                this.isLoading = false;
+            },
+            errorMessage => {
+                console.log(errorMessage);
+                this.error = errorMessage;
+                this.isLoading = false;
+            }
+        );        
         
         form.reset();
     }
