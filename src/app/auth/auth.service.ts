aba0a9f963ef6d6d5c2aeedafa7c22b5183fb445
Mayank <mayankgupta14042000@gmail.com>
2022-04-10 12:17:49 +0530

    Auto login feature

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { tap, catchError } from "rxjs/operators";
 import { BehaviorSubject, throwError } from "rxjs";
 import { User } from "./user.model";
+import { Router } from "@angular/router";
 
 
 export interface AuthResponseData {
@@ -19,7 +20,7 @@ export interface AuthResponseData {
 export class AuthService {
     user = new BehaviorSubject<User>(null);
 
-   constructor(private http: HttpClient) {}
+   constructor(private http: HttpClient, private router: Router) {}
    
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>(
@@ -47,14 +48,34 @@ export class AuthService {
         }));
     }
 
+    autoLogin() {
+        const userData: {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if (!userData) {
+            return;
+        }
+
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+        
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
+    }
+
     logout() {
         this.user.next(null);
+        this.router.navigate(['/auth']);
     }
 
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000 );
-            const user = new User(email, userId, token, expirationDate);
-            this.user.next(user);
+        const user = new User(email, userId, token, expirationDate);
+        this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse) {
